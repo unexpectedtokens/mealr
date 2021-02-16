356105547ca42d6cbbd7d38a858ade8406c825a8
daniel <dejong543@gmail.com>
2021-02-16 12:48:17 +0100

    modified build file

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #This file contains the steps which are needed to deploy this application
 #moves into the client directory where the react client resides and builds it
-cd client && npm run build
-
+cd client
+npm install
+npm run build
+rm -rf node_modules
 #moves back into the main directory and compiles the golang backend to binary
 cd ..
 go build cmd/main.go
