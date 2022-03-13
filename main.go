7bdc8f302c9208aaf58341a435528c14d18a1f10
daniel <dejong543@gmail.com>
2022-03-13 18:42:54 +0100

    staging 5000 v3

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 var migrationFileName = flag.String("filename", "initial_migration", "name of the specific file to parse for migration")
 
 func main() {
-	fmt.Println(21 << 20)
+
 	logging.InitLogging()
 
 	mode := os.Getenv("MODE")
