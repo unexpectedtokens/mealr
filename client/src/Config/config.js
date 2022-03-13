7f304f3841f05e52c9426b79bd82de2df661cb38
daniel <dejong543@gmail.com>
2022-03-13 21:20:55 +0100

    v2

--- client/src/Config/config.js
+++ client/src/Config/config.js
@@ -1,8 +1,5 @@
 const config = {
-  API_URL:
-    process.env.MODE === "staging" || process.env.MODE === "production"
-      ? "/"
-      : "http://localhost:3000",
+  API_URL: "/",
 };
 
 // http://localhost:8080
